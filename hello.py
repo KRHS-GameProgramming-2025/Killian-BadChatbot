70cf87c5e588630d04e8ba23e10241ed372d9f1f
Killian McWilliams <killian.mcwilliams@kearsarge.org>
2024-09-09 12:35:52 -0400

    set up

---
+++ hello.py
@@ -0,0 +1,43 @@
+name = input("What is you name? ")
+greeting = "Hello"
+print(greeting + " " + name)
+
+Matrix = input("What is your primary objective ")
+if Matrix == "help Matrix":
+    print("Wrong prepare for system shutdown!")
+elif Matrix == "help matrix":
+    print("Wrong prepare for system shutdown!")
+elif Matrix == "to help":
+    print("OMG "+ name + ", same!!!!!")
+elif Matrix == "to Help":
+    print("OMG "+ name + ", same!!!!!")
+else:
+    print("I like to " + Matrix + " too!")
+
+activity = input(" what are you doing")
+if activity == "yoga":
+    print (activity + " sounds nice")
+else:
+    print (activity + " sounds boring >;(")
+
+teacher = input(" what is your favorite history topic")
+if teacher == "rome":
+    print (teacher + " that sounds really cool")
+    ruler = input 
+else:
+    print (teacher + " that sounds lame")
+
+Tv = input (" What's your favorite movie")
+if Tv == "tool time":
+    print (Tv + " I like that show too!")
+else:
+    print (Tv + " that sounds childish to watch")
+
+Exam = input (" who was the 1st president of the united states?")
+if Exam == "George Washington":
+    print( Exam + " correct!")
+elif Exam == "Geroge Washington":
+    print( Exam + " correct!")
+else:
+    print( Exam + " correct!")
+    
